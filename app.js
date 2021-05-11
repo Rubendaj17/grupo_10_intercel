469b18a7a65a87925e7eac243ec21b57fafa2770
Alex Bulo <bullorinia@gmail.com>
2021-05-11 01:02:46 -0300

    Product Detail Mobile 80%

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.listen(3000, ()=> {
 app.get('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 })
+
+app.get("/product-detail", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/product-detail.html"))
+})
+
 app.get("/redes", (req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/redes.html"))
 })
