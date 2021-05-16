dd0f92dd13bfde4e6e55af82f84ed9d84fb975bd
Rubendaj17 <rubendaj@msn.com>
2021-05-16 11:29:10 -0500

    Agregrando carrito de compras

--- app.js
+++ app.js
@@ -29,3 +29,7 @@ app.get("/login", (req,res)=>{
 app.get("/register", (req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/register.html"))
 })
+
+app.get("/cart", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/productCart.html"))
+})
