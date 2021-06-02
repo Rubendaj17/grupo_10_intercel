50783fae7fa74bf121a2bbafc5b548dacb74d55e
unknown <justiiturraspe@gmail.com>
2021-06-02 04:10:48 -0300

    css a edit y new

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,13 +7,11 @@ let usersController = {
     register: (req,res)=>{
         res.render('register')
     },
-    newProduct: (req, res)=>{
-        res.sendFile(path.resolve(__dirname,"../views/newProduct.html"))
+    newProduct: (req,res)=>{
+        res.render('newProduct')
+        //res.sendFile(path.resolve(__dirname,"../views/newProduct.ejs"))
     },
-    editProduct: (req, res)=>{
-        res.sendFile(path.resolve(__dirname,"../views/editProduct.ejs"))
-    },
-    editProduct: (req, res)=>{
+    editProduct (req, res){
         let product={
             model: "X",
             description: "blanco",
@@ -21,8 +19,8 @@ let usersController = {
             high: 8,
             width: 3.9,
         }
-        return res.render('editProduct', {'product': product});
-    },
+        res.render('editProduct', {'product': product})
+    }
 }
 
 module.exports = usersController
