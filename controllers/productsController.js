1528488775d88ac29b3a640244f5749ce60516ec
unknown <justiiturraspe@gmail.com>
2021-06-04 03:58:56 -0300

    separar las vistas en carpetas

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,11 +2,24 @@ const path = require('path');
 
 let productController = {
     list: (req,res)=>{
-        res.render('products')
+        res.render('products/products')
     },
-
     detail:(req,res)=>{
-        res.render('productDetail')
+        res.render('products/productDetail')
+    },
+    newProduct: (req,res)=>{
+        res.render('products/newProduct')
+        //res.sendFile(path.resolve(__dirname,"../views/newProduct.ejs"))
+    },
+    editProduct (req, res){
+        let product={
+            brand: "samsung",
+            model: "A20",
+            color: "blanco",
+            price: 576,
+            ram: 8,
+        }
+        res.render('products/editProduct', {'product': product})
     }
 
 }
