97f3a5b6a51edad7d49a009634d1a84d748a7948
Ale Bullo <bullorinia@gmail.com>
2021-07-29 00:44:36 -0300

    try/catch y comments en Create and Destroy controllers

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -15,29 +15,37 @@ let productController = {
         res.render('products/products', {productList, brand})
     },
     adminList: async (req,res)=>{
-        
-        const brandList = await db.Brand.findAll( {
-            order: [['id', 'ASC']]
-        }) 
-        
-        const modelList = await db.Model.findAll({
-            include: ['brand'],
-            order: [['id_brand', 'ASC']]
-        }) 
-
-        const productList = await db.Cellphone.findAll({
-            include: [{
-                association: 'model',
-            },
-            {
-                association: 'color',
-            },
-            {
-                association: 'ram',
-            }]
-        })
- 
-        res.render('products/adminList', {productList, brandList, modelList})
+        try {
+            //busca todas las Brands
+            const brandList = await db.Brand.findAll( {
+                order: [['id', 'ASC']]
+            }) 
+            
+            //busca todos los Models
+            const modelList = await db.Model.findAll({
+                include: ['brand'],
+                order: [['id_brand', 'ASC']]
+            }) 
+    
+            //busca todos los Celulares
+            const productList = await db.Cellphone.findAll({
+                include: [{
+                    association: 'model',
+                },
+                {
+                    association: 'color',
+                },
+                {
+                    association: 'ram',
+                }]
+            })
+     
+            res.render('products/adminList', {productList, brandList, modelList})
+            
+        } catch (error) { //si hay error, muestra el error en consola y vista de error
+            console.log(error);
+            res.status(500).render('soon', {error})
+        }
 
         
     },
@@ -111,137 +119,152 @@ let productController = {
 
     newProductForm: async (req,res)=>{
         const { brand, model } = req.query
-
-        let brandToUse = await db.Brand.findOne({
-            where:{name:brand}
-        })
-        if (!brandToUse){
-            brandToUse = {
+        try {
+            //busca la Marca segun el query string
+            let brandToUse = await db.Brand.findOne({
+                where:{name:brand}
+            })
+            //si No existe, arma objeto de marca e imagen vacios para la vista
+            if (!brandToUse){
+                brandToUse = {
                     name:'',
                     imageToUse: ''
                 }
-        }else{
-            brandToUse.imageToUse = '/images/logos/'+brandToUse.logo
-        } 
-        
-        let modelToUse = await db.Model.findOne({
-            where:{model}
-        })        
-        if (!modelToUse){
-            modelToUse = {
+            }else{ //si existe, modifica el path para la imagen para usar en la vista
+                brandToUse.imageToUse = '/images/logos/'+brandToUse.logo
+            } 
+            
+            //busca el Modelo segun el query string
+            let modelToUse = await db.Model.findOne({
+                where:{model}
+            })        
+            //si No existe, arma objeto de marca e imagen vacios para la vista
+            if (!modelToUse){
+                modelToUse = {
                     model:'',
                     imageToUse:''
                 }
-        } else{
-            modelToUse.imageToUse = '/images/cellphones/models/'+modelToUse.mainImage
-        } 
-
-        const ramList = await db.Ram.findAll()
-        const colorList = await db.Color.findAll()
-        
-        res.render('products/newProduct', {brandToUse, modelToUse, ramList, colorList})
-    },
-    createNewProduct: async (req, res) => {
-        let errors = validationResult(req)
-
-        if (!errors.isEmpty()){
-            let brand = {
-                name: req.body.brand,
-                imageToUse:''
-            }
+            } else{//si existe, modifica el path para la imagen para usar en la vista
+                modelToUse.imageToUse = '/images/cellphones/models/'+modelToUse.mainImage
+            } 
             
-            const isBrand = await db.Brand.findOne({
-                where:{name:brand.name}
-            })
-            
-            if(isBrand){
-                brand.imageToUse = '/images/logos/'+isBrand.logo
-            }
-
-            let model = {
-                name: req.body.model,
-                imageToUse:''
-            }
-            
-            const isModel = await db.Model.findOne({
-                where:{model:model.name}
-            })
-            if(isModel){
-                model.imageToUse = '/images/cellphones/models/'+isModel.mainImage
-            }
-
-            const price = req.body.price
-            const offer = req.body.offer ? req.body.offer : 0
-            const color = req.body.color
-            const ram = req.body.ram
-            
-            const userChoices = {brand, model, price, offer, color, ram}
-            
+            //busca el todas las opciones de Ram y Color
             const ramList = await db.Ram.findAll()
             const colorList = await db.Color.findAll()
             
-            res.render('products/newProduct', {errors:errors.mapped(), userChoices, ramList, colorList})
+            res.render('products/newProduct', {brandToUse, modelToUse, ramList, colorList})
             
-            return
+        } catch (error) { //si hay error, muestra el error en consola y vista de error
+            console.log(error);
+            res.status(500).render('soon', {error})
         }
-
-
-        // busca Marca elegida por el usuario
-        let brandToSave = await db.Brand.findOne({
-            where: { name:req.body.brand }
-        })
-
-        // si No existe crea la marca nueva en la BdD
-        if(!brandToSave){
-
-            brandToSave = await db.Brand.create(
-                { 
+    },
+    createNewProduct: async (req, res) => {
+        try {
+            let errors = validationResult(req)
+            //Si ha errores, guarda valores del usuario para volver a mostrar
+            if (!errors.isEmpty()){
+                let brand = {
                     name: req.body.brand,
-                    logo: req.files.logo[0].filename
+                    imageToUse:''
                 }
-            )
-        }
-        // guarda el id de la marca
-        const idBrand = brandToSave.id 
-        
-        // busca Modelo elegido por el usuario
-        let modelToSave = await db.Model.findOne({
-            where: { model:req.body.model }
-        })
-        
-        // si No existe crea el modelo nuevo en la BdD
-        if(!modelToSave){
-            modelToSave = await db.Model.create(
-                { 
-                    idBrand,
-                    model: req.body.model,
-                    mainImage: req.files.modelMainImage[0].filename
+                
+                const isBrand = await db.Brand.findOne({
+                    where:{name:brand.name}
+                })
+                
+                if(isBrand){
+                    brand.imageToUse = '/images/logos/'+isBrand.logo
                 }
+    
+                let model = {
+                    name: req.body.model,
+                    imageToUse:''
+                }
+                
+                const isModel = await db.Model.findOne({
+                    where:{model:model.name}
+                })
+                if(isModel){
+                    model.imageToUse = '/images/cellphones/models/'+isModel.mainImage
+                }
+    
+                const price = req.body.price
+                const offer = req.body.offer ? req.body.offer : 0
+                const color = req.body.color
+                const ram = req.body.ram
+                
+                const userChoices = {brand, model, price, offer, color, ram}
+                
+                const ramList = await db.Ram.findAll()
+                const colorList = await db.Color.findAll()
+                
+                res.render('products/newProduct', {errors:errors.mapped(), userChoices, ramList, colorList})
+                
+                return //corta el codigo para que no continue luego del if
+            }
+    
+            // si no hay errores, busca Marca elegida por el usuario
+            let brandToSave = await db.Brand.findOne({
+                where: { name:req.body.brand }
+            })
+    
+            // si No existe crea la marca nueva en la BdD
+            if(!brandToSave){
+    
+                brandToSave = await db.Brand.create(
+                    { 
+                        name: req.body.brand,
+                        logo: req.files.logo[0].filename
+                    }
                 )
             }
+            // guarda el id de la marca
+            const idBrand = brandToSave.id 
             
-        //guarda toda la info del nuevo celular
-
-        const idModel = modelToSave.id
+            // busca Modelo elegido por el usuario
+            let modelToSave = await db.Model.findOne({
+                where: { model:req.body.model }
+            })
+            
+            // si No existe crea el modelo nuevo en la BdD
+            if(!modelToSave){
+                modelToSave = await db.Model.create(
+                    { 
+                        idBrand,
+                        model: req.body.model,
+                        mainImage: req.files.modelMainImage[0].filename
+                    }
+                    )
+                }
                 
-        const idColor = req.body.color
-        
-        const idRam = req.body.ram
-
-        const price = Number(req.body.price)
-
-        const offer = Number(req.body.offer)
-        
-        const imageOne = req.files.imageOne[0].filename
-        const imageTwo = req.files.imageTwo[0].filename
-        const imageThree = req.files.imageThree[0].filename
-
-        const newProduct = {idModel, idColor, idRam, price, offer, imageOne, imageTwo, imageThree}
+            //guarda toda la info del nuevo celular
+    
+            const idModel = modelToSave.id
+                    
+            const idColor = req.body.color
             
-        //crea el nuevo celular en la BdD
-        const newCellphone = await db.Cellphone.create( newProduct )
-        // res.redirect('products/adminList')
-        res.redirect('/products/'+newCellphone.id)
+            const idRam = req.body.ram
+    
+            const price = Number(req.body.price)
+    
+            const offer = Number(req.body.offer)
+            
+            const imageOne = req.files.imageOne[0].filename
+            const imageTwo = req.files.imageTwo[0].filename
+            const imageThree = req.files.imageThree[0].filename
+    
+            const newProduct = {idModel, idColor, idRam, price, offer, imageOne, imageTwo, imageThree}
+                
+            //crea el nuevo celular en la BdD
+            const newCellphone = await db.Cellphone.create( newProduct )
+            // res.redirect('products/adminList')
+            res.redirect('/products/'+newCellphone.id)
+            
+        } catch (error) {//si hay error, muestra el error en consola y vista de error
+            console.log(error);
+            res.status(500).render('soon')
+        }
     },
 
     editProductForm (req, res){
@@ -278,11 +301,20 @@ let productController = {
        
     },
 
-    destroy(req, res){
+    destroy: async (req, res) => {
         const id = req.params.id;
-        productsModel.destroy(id);
+
+        try {
+            const deleteCellphone = await db.Cellphone.destroy({
+                where: {id}
+            })
+            
+            res.redirect('/products/list' )
+        } catch (error) {//si hay error, muestra el error en consola y vista de error
+            console.log(error);
+            res.status(500).render('soon')
+        }
         
-        res.redirect('/products/list' )
     },
     
 }
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -37,7 +37,6 @@ productsRouter.get("/list/:brand", productsController.brandList)
 // formulario crear y envio de creacion
 productsRouter.get("/create", productsController.newProductForm);
 productsRouter.post("/create", upload.fields([{name:'logo'},{name:'modelMainImage'},{name:'imageOne'},{name:'imageTwo'},{name:'imageThree'}]), validationNewProduct, foldersMiddleware, imageMiddleware, productsController.createNewProduct);
-
 productsRouter.delete("/:id", productsController.destroy);
 
 // detalle producto
