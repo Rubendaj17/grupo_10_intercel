d84cb67ed6b4762effc603aa4ce1425276871bf9
unknown <rubendaj@msn.com>
2021-08-31 18:25:45 -0500

    validaciones edit

--- public/js/editValidation.js
+++ public/js/editValidation.js
@@ -2,18 +2,39 @@ window.addEventListener('load', function(){
 
     const form = document.querySelector('.editForm')
     
-    const inputPrice = document.querySelector('#precio.inputControl')
+    const inputPrice = document.querySelector('#price.inputControl')
     const errorPrice = document.querySelector('#priceError')
 
+    const inputImageOne = document.querySelector('#imageOne.inputControl')
+    const errorImageOne = document.querySelector('#imageOneError')
 
+    const inputImageTwo = document.querySelector('#imageTwo.inputControl')
+    const errorImageTwo = document.querySelector('#imageTwoError')
+
+    const inputImageThree = document.querySelector('#imageThree.inputControl')
+    const errorImageThree = document.querySelector('#imageThreeError')
+    
+    
     function isNumeric(n) {
         return !isNaN(parseFloat(n)) && isFinite(n);
     }
 
     function resetErrors(){
         errorPrice.innerHTML = ""
+        errorImageOne.innerHTML = ""
 
     }
+
+    function validateExt(file){
+        const allowedExt = ['.jpg', '.jpeg', '.png', '.svg']
+        const format = "." + file.split('.').pop()
+        if (allowedExt.includes(format)){
+            return true
+        }
+        return false
+        
+    }
+
 
     form.addEventListener('submit', (e)=> {
         let hasErrors = false
@@ -25,7 +46,40 @@ window.addEventListener('load', function(){
             errorPrice.innerHTML = "Por favor ingrese un precio válido (número mayor a 100) "
             hasErrors = true
             inputPrice.focus()
-        }       
+        } 
+
+        // images
+
+        if (inputImageOne.value){
+            validateExt(inputImageOne.value)
+            
+            if(validateExt(inputImageOne.value) == false){
+                errorImageOne.innerHTML = "Por favor ingrese únicamente imagenes"
+                hasErrors = true
+                inputImageOne.focus()
+            }                                
+        }
+
+        if (inputImageTwo.value){
+            validateExt(inputImageTwo.value)
+            
+            if(validateExt(inputImageTwo.value) == false){
+                errorImageTwo.innerHTML = "Por favor ingrese únicamente imagenes"
+                hasErrors = true
+                inputImageTwo.focus()
+            }                                
+        }
+
+        if (inputImageThree.value){
+            validateExt(inputImageThree.value)
+            
+            if(validateExt(inputImageThree.value) == false){
+                errorImageThree.innerHTML = "Por favor ingrese únicamente imagenes"
+                hasErrors = true
+                inputImageThree.focus()
+            }                                
+        }
+        
         
         if(hasErrors){
             e.preventDefault()
@@ -33,6 +87,8 @@ window.addEventListener('load', function(){
 
 
     })
+
+
 
 
 })
--- src/middlewares/validationEditProduct.js
+++ src/middlewares/validationEditProduct.js
@@ -1,15 +1,27 @@
 const {body} = require('express-validator')
+const { isFileImage } = require('../helpers/file')
 
 const validationEditProduct = [
         
     //validacion price
     body('price')
-    .notEmpty()
-    .withMessage('Debe ingresar un precio')
-    .isNumeric()
-    .withMessage('Debe ingresar un número')
-    .bail(),
-
+        .notEmpty()
+        .withMessage('Debe ingresar un precio')
+        .isNumeric()
+        .withMessage('Debe ingresar un número')
+        .bail(), 
+    
+    body('imageTwo')
+        .custom((value,{ req }) =>{
+            const { file } = req
+        
+            if(!file){
+                throw new Error ("Por favor ingrese una imagen")
+            }
+        
+        
+            return true
+        })
 ]
 
 module.exports = validationEditProduct
