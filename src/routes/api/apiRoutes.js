5e06b33dd1b648b8cf122c07baa199fc16727515
Ale Bullo <bullorinia@gmail.com>
2021-09-03 22:13:50 -0300

    api users para ultimo sprint

--- src/routes/api/apiRoutes.js
+++ src/routes/api/apiRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const apiController = require('../../controllers/api/apiController');
+const apiUserController = require('../../controllers/api/apiUserController');
 const apiRouter = express.Router()
 
 
 apiRouter.get('/search', apiController.searchProduct)
 
-apiRouter.get('/users', apiController.register)
+apiRouter.get('/users', apiUserController.allUsers)
+
+// apiRouter.get('/users/:id', apiController.uniqueUser)
+
+// cambiar nombre de ruta para js front Login/register
+apiRouter.get('/user', apiController.register)
 
 apiRouter.get('/brands', apiController.brands)
 
