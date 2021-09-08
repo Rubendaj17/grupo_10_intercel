217c4fb2726173a7ecc9232f3c5d7f9ec90c10aa
Ale Bullo <bullorinia@gmail.com>
2021-09-07 22:51:31 -0300

    api products

--- src/controllers/api/apiProductController.js
+++ src/controllers/api/apiProductController.js
@@ -4,52 +4,47 @@ let apiProductController = {
     allProducts: async (req,res) => {
         // pedir info a BDD
         
-        const productList = await db.Cellphone.findAndCountAll({
-            include: ['model','color','ram',],
-            
-        })
-
-        const countByModel = await db.Cellphone.findAndCountAll({
-            include: ['model','color','ram',],
-            group:['model.model']
-        })
-
-        let usersToReturn = []
-
-        userList.rows.forEach( u => {
-
-            let infoToReturn = {
-                id: u.dataValues.id,
-                name: u.dataValues.name,
-                email: u.dataValues.email,
-                detail: `http://localhost:3000/api/user/${u.dataValues.id}`
-            }
-
-            usersToReturn.push(infoToReturn)
-
-        })
-
-
-        return res.json() 
-
-
-
 
         try {
             
-            // console.log(usersToReturn);
+            const productsAll = await db.Cellphone.findAndCountAll({
+                include: ['model','color','ram'],
+                attributes: ['id','price', 'offer']
+        })
+        let products = []
+
+        productsAll.rows.forEach( product =>{
+            let info = {
+                'id': product.id,
+                'price': product.price,
+                'offer': product.offer,
+                'model': product.model.model,
+                'description': product.model.description,
+                'color': product.color.name,
+                'ram': product.ram.storage,
+                'detail': `http://localhost:3000/api/products/${product.id}`
+            }
+            products.push(info)
+        })
+
+        const models = await db.Model.findAll({
+            include: ['cellphones'],    
+        })
+        
+        let countByModel = {}
+        models.forEach( model => {
+            countByModel[model.model] = model.cellphones.length
+        })
+
 
             res.status(200).json({
-                count: userList.count,
-                users: usersToReturn
+                count: productsAll.count,
+                countByModel,
+                products: products
             })
             
         } catch (error) {
             res.status(500).json({
-                meta:{
-                    status:"error",
-                    total: 0
-                },
                 error:{
                     msg: "No se pudo conectar a la Base de Datos",
                     error
@@ -57,6 +52,12 @@ let apiProductController = {
     
             })
         }
+
+        
+
+
+
+
 
     }
 }
