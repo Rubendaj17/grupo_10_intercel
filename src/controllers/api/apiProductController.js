f50d3cb7398a9efab77480aa57eb3db79c7b32d4
unknown <rubendaj@msn.com>
2021-09-09 22:37:31 -0500

    Mergeando Test

--- src/controllers/api/apiProductController.js
+++ src/controllers/api/apiProductController.js
@@ -29,6 +29,7 @@ let apiProductController = {
                         models
                     }
             })
+            
 // calcular total productos
             const productsAll = await db.Cellphone.findAndCountAll({
                 include: [{
